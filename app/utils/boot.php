2d456538c002925413649cb4693e220ba90a8b6f
Asao <kamei@workspot.jp>
2015-02-18 17:55:21 +0900

    clean up some comments etc.

--- app/utils/boot.php
+++ app/utils/boot.php
@@ -10,12 +10,13 @@ use Tuum\Web\Web;
  */
 return function( array $config ) {
 
-    // -----------------------------------------------
-    // default configuration
-    // -----------------------------------------------
-    
+    /**
+     * default configuration.
+     */
+
     $app_root       = dirname(__DIR__);
     $project_root   = dirname($app_root);
+    $tuum_scripts   = $project_root.'/vendor/tuum/web/scripts';
     $default_config = [
         
         // default routes file to set up router.
@@ -38,49 +39,43 @@ return function( array $config ) {
     ];
     $config += $default_config;
 
-    // -----------------------------------------------
-    // build web application, $app
-    // -----------------------------------------------
+    /**
+     * build and configure web application, $app.
+     */
 
     /** @var Web $app */
-    $tuum_scripts = $project_root.'/vendor/tuum/web/scripts';
-    $app = include($tuum_scripts.'/boot.php');
-
-    // -----------------------------------------------
-    //  configure $app
-    // -----------------------------------------------
-
     // use Tuum's basic web configuration.
+
+    $app = include($tuum_scripts.'/boot.php');
     $app->configure($tuum_scripts.'/configure');
 
     // use the config dir's configure.
+
     $config_dir = $config[App::CONFIG_DIR];
     $app->configure($config_dir . '/configure');
 
-    // -----------------------------------------------
     // set up directories
-    // -----------------------------------------------
-    
+
     $app->set(App::CONFIG_DIR,   $config[App::CONFIG_DIR]);
     $app->set(App::TEMPLATE_DIR, $config[App::TEMPLATE_DIR]);
     $app->set(App::DOCUMENT_DIR, $config[App::DOCUMENT_DIR]);
     $app->set(App::VAR_DATA_DIR, $config[App::VAR_DATA_DIR]);
     $app->set(App::DEBUG,        $config[App::DEBUG]);
 
-    // -----------------------------------------------
+    /**
+     * set up stacks and routes.
+     */
+
     // set up stacks
-    // -----------------------------------------------
-    
+
     $stacks = $app->get('stacks');
     foreach($stacks as $stack) {
         $app->push($app->get($stack));
     }
 
-    // -----------------------------------------------
     // read the routes.
     // the route files are out of the config directory. 
-    // -----------------------------------------------
-    
+
     $route_files = (array) $config['routes'];
     foreach($route_files as $routes ) {
         $app->push($app->execute($routes));
