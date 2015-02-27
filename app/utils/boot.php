3f80669d96250be57c127e00fb42ae705123c109
Asao <kamei@workspot.jp>
2015-02-28 08:23:17 +0900

    use getApp() method.

--- app/utils/boot.php
+++ app/utils/boot.php
@@ -11,43 +11,29 @@ use Tuum\Web\Application;
 return function( array $config ) {
 
     /**
-     * default configuration.
-     */
-
-    $tuum_scripts   = dirname(dirname(__DIR__)).'/vendor/tuum/web/scripts';
-
-    /**
      * build and configure web application, $app.
      */
 
-    /** @var Application $app */
-    $app = include($tuum_scripts.'/boot.php');
+    $app = Web::getApp($config);
 
-    // use Tuum's basic web configuration.
-    
-    $app->configure($tuum_scripts.'/configure');
-
-    // set up directories
-    
-    $app->set(Web::CONFIG_DIR,   $config[Web::CONFIG_DIR]);
-    $app->set(Web::TEMPLATE_DIR, $config[Web::TEMPLATE_DIR]);
-    $app->set(Web::DOCUMENT_DIR, $config[Web::DOCUMENT_DIR]);
-    $app->set(Web::VAR_DATA_DIR, $config[Web::VAR_DATA_DIR]);
-    $app->set(Web::DEBUG,        $config[Web::DEBUG]);
+    /**
+     * configure the application.
+     */
 
     // use the config directory's configure.
     
     $config_dir = $config[Web::CONFIG_DIR];
+    
     $app->configure($config_dir . '/configure');
-    
+
     // debug configuration
-    
-    if($config[Web::DEBUG]) {
+
+    if($config[Web::DEBUG]) {  
         $app->configure($config_dir.'/configure-debug');
     }
-    
+
     // environment specific configuration
-    
+
     /** @noinspection PhpIncludeInspection */
     if( $environment = (array) include($config['environment'])) {
         foreach($environment as $env) {
