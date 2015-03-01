5d1b2f29bc8e19ba721ce7b2bd8bda44bf2d1a1d
Asao <kamei@workspot.jp>
2015-03-01 18:22:55 +0900

    pre compile Monolog classes.

--- app/utils/compile-includes.php
+++ app/utils/compile-includes.php
@@ -6,6 +6,10 @@
 
 use Aura\Session\SessionFactory;
 use ClassPreloader\ClassLoader;
+use Monolog\Handler\BrowserConsoleHandler;
+use Monolog\Handler\FingersCrossedHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Tuum\Web\Application;
 use Tuum\Web\Psr7\RequestFactory;
 use Tuum\Web\Web;
@@ -20,7 +24,10 @@ $config = ClassLoader::getIncludes(function( ClassLoader $loader) {
     $config['environment'] = ['compiled'];
 
     $boot = include(__DIR__ . '/boot.php');
-    
+
+    /**
+     * load $app, request, and response. 
+     */
     /** @var Closure $boot */
     /** @var Application $app */
     $app  = $boot($config);
@@ -28,15 +35,32 @@ $config = ClassLoader::getIncludes(function( ClassLoader $loader) {
     $request  = RequestFactory::fromPath('no-such');
     $response = $request->respond()->asForbidden();
     
+    /**
+     * try session stuff.
+     */
     $session_factory = new SessionFactory;
     $session         = $session_factory->newInstance($_COOKIE);
     $segment = $session->getSegment('TuumPHP/WebApplication');
     $token = $session->getCsrfToken('sample-token');
     $flash   = $segment->getFlash('flashed');
-
+    /**
+     * view
+     */
     $view     = include_once(dirname(dirname(__DIR__)).'/vendor/tuum/web/scripts/compiled/view.php');
     $control  = include_once(dirname(dirname(__DIR__)).'/vendor/tuum/web/scripts/compiled/controller.php');
     $control->__invoke($request);
+
+    /**
+     * monolog
+     */
+    $logger  = new Logger('compiled-includes');
+    $logger->pushHandler(
+        new FingersCrossedHandler(new StreamHandler($config[Web::VAR_DATA_DIR].'/log', Logger::DEBUG))
+    );
+    $logger->pushHandler(
+        new BrowserConsoleHandler(Logger::DEBUG)
+    );
+
     return $config;
 
 });
