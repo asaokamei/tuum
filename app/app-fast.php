ada6abc6c6466dee7b7e56eb2aae2de08e88025a
Asao <kamei@workspot.jp>
2015-04-16 13:22:50 +0900

    fixed minimum app page and enabled xh-profiling.

--- app/app-fast.php
+++ app/app-fast.php
@@ -8,6 +8,9 @@ use Tuum\Web\Web;
 
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
+$xhProf_limit = isset($xhProf_limit) ? $xhProf_limit: '1.0';
+include __DIR__ . '/utils/boot-xhprof.php';
+
 #
 # build and configure $app.
 #
