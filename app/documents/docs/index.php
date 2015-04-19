6c86e6f65c39d7b122fd2096b9aab12156309e26
Asao <kamei@workspot.jp>
2015-04-20 06:41:30 +0900

    add text and raw link.

--- app/documents/docs/index.php
+++ app/documents/docs/index.php
@@ -23,8 +23,8 @@
 <ul>
     <li><a href="tuum.html" >html file</a></li>
     <li><a href="non-such.html">not found</a></li>
-    <li><a href="tuum.txt" >text file</a></li>
-    <li><a href="marked" >markdown file</a> (<a href="marked.md" >markdown file</a>)</li>
+    <li><a href="tuum" >text file</a> (<a href="tuum.txt" >raw text file</a>)</li>
+    <li><a href="marked" >markdown file</a> (<a href="marked.md" >raw markdown file</a>)</li>
     <li><a href="errors" >php exception thrown</a></li>
 </ul>
 
