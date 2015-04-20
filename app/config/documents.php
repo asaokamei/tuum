c4b53a2886522a0a79879e40b98267150496e3f3
Asao <kamei@workspot.jp>
2015-04-21 07:04:22 +0900

    DocView to use before filters;
    set layout for docs in before filters.

--- app/config/documents.php
+++ app/config/documents.php
@@ -7,5 +7,8 @@ use Tuum\Web\Web;
 $stack  = $web->getDocViewStack($web->app_dir.'/documents');
 $stack->enable_raw = true;
 $stack->setRoot('docs*');
+$stack->setBeforeFilter(function() use($web) {
+    $web->getViewEngine()->setLayout('layout/docs-layout');
+});
 
 return $stack;
--- app/views/layout/docs-layout.php
+++ app/views/layout/docs-layout.php
@@ -1,20 +1,16 @@
 <?php /** @var \Tuum\View\Renderer $this */ ?>
 <?php /** @var \Tuum\Web\View\Value $view */ ?>
 
-<?php $this->setLayout('layout/docs-layout'); ?>
+<?php $this->setLayout('layout/layout'); ?>
 
 <?php $view->data['current'] = 'maps'; ?>
 
-<?php $this->blockAsSection('docs/sub-menu', 'sub-menu', ['current'=>'index']); ?>
+<?php $this->blockAsSection('layout/docs-sub-menu', 'sub-menu', ['current'=>'index']); ?>
 
 <?php $this->startSection() ?>
 <li><a href="/docs/index" >Documents</a></li>
 <li class="active">top</li>
 <?php $this->endSectionAs('breadcrumb'); ?>
 
-<!Doctype html>
-<html>
-<head>
-    <title>Tuum is here</title>
-</head>
-<body>
+<?= $this->getContent();?>
+
