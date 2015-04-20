3b15e64e57a6e9fa21c6c92855dde88a6c407c73
Asao <kamei@workspot.jp>
2015-04-21 06:25:34 +0900

    use include/ob_start to render php, not to use the renderer.

--- app/documents/docs/index.php
+++ app/documents/docs/index.php
@@ -1,21 +1,3 @@
-<?php /** @var \Tuum\View\Renderer $this */ ?>
-<?php /** @var \Tuum\Web\View\Value $view */ ?>
-
-<?php $view->data['current'] = 'maps'; ?>
-
-<?php $this->blockAsSection('docs/sub-menu', 'sub-menu', ['current'=>'index']); ?>
-
-<?php $this->startSection() ?>
-<li><a href="/docs/index" >Documents</a></li>
-<li class="active">top</li>
-<?php $this->endSectionAs('breadcrumb'); ?>
-
-<!Doctype html>
-<html>
-<head>
-    <title>Tuum is here</title>
-</head>
-<body>
 <h1>PHP View File</h1>
 <p>This is directly rendered from a PHP file.</p>
 
@@ -27,6 +9,3 @@
     <li><a href="marked" >markdown file</a> (<a href="marked.md" >raw markdown file</a>)</li>
     <li><a href="errors" >php exception thrown</a></li>
 </ul>
-
-</body>
-</html>
