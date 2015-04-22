50ea6c8dacca42c9ac03e8f671050fbcb73593f2
Asao <kamei@workspot.jp>
2015-04-23 00:08:11 +0900

    allow to set a view-composer closure for setting up renderer just before rendering. streamlined some layout for documents.

--- app/views/layout/docs-layout.php
+++ app/views/layout/docs-layout.php
@@ -1,32 +1,73 @@
 <?php
+/**
+ * This is an interim layout for Documents rendered by DocView.
+ */
+
 /** @var \Tuum\View\Renderer $this */
 /** @var \Tuum\Web\View\Value $view */
 
-$current = basename($view->uri->getPath());
-$current = isset($current) ? $current : 'index';
-$current = $current === 'license' ? 'index' : $current;
-
-$titleList = [
-    'index' => 'Documents Top',
-    'quick-install' => 'Installation',
-    'quick-routing' => 'Routes File',
+$file_name   = isset($file_name) ? $file_name: 'index';
+$titleList   = [
+    'index'            => 'Documents Top',
+    'quick-install'    => 'Installation',
+    'quick-routing'    => 'Routes File',
     'quick-controller' => 'Controller and View',
     '' => '',
 ];
-$title = isset($titleList[$current]) ? $titleList[$current] : 'Documents';
+$bread_title = isset($titleList[$file_name]) ? $titleList[$file_name] : 'Documents';
+$activate    = function($case) use($file_name) {
+    return $case === $file_name ? ' class="active"': '';
+};
 
+/**
+ * set up the master layout. 
+ */
+$this->setLayout('layout/layout'); 
+$view->data['current'] = 'maps'; 
 ?>
 
-<?php $this->setLayout('layout/layout'); ?>
-
-<?php $view->data['current'] = 'maps'; ?>
-
-<?php $this->blockAsSection('layout/docs-sub-menu', 'sub-menu', ['current'=>$current]); ?>
-
-<?php $this->startSection() ?>
+<?php
+/**
+ * start of BreadCrumb section.
+ */
+$this->startSection() 
+?>
 <li><a href="/docs/index" >Documents</a></li>
-<li class="active"><?= $title ?></li>
+<li class="active"><?= $bread_title ?></li>
 <?php $this->endSectionAs('breadcrumb'); ?>
 
-<?= $this->getContent();?>
+<?php
+/**
+ * start of sub-menu section.
+ */
+$this->startSection() 
+?>
+<ul class="nav nav-pills nav-stacked">
+    <li role="presentation" <?= $activate('index')?>><a href="/docs/index" >Document Top</a></li>
+</ul>
+
+<p class="nav-header">Quick Start</p>
+<ul class="nav nav-pills nav-stacked">
+    <li<?= $activate('quick-install')?>><a href="/docs/quick-install" >Installation</a></li>
+    <li<?= $activate('quick-routing')?>><a href="/docs/quick-routing" >Routes File</a></li>
+    <li<?= $activate('quick-controller')?>><a href="/docs/quick-controller" >Controller and View</a></li>
+</ul>
+
+<p class="nav-header">Manual</p>
+<ul class="nav nav-pills nav-stacked">
+    <li<?= $activate('manual-http')?>><a href="#" >Request and Response</a></li>
+    <li<?= $activate('manual-middleware')?>><a href="#" >Middleware</a></li>
+    <li<?= $activate('manual-view')?>><a href="#" >View</a></li>
+    <li<?= $activate('manual-forms')?>><a href="#" >Form Helper</a></li>
+    <li<?= $activate('manual-router')?>><a href="#" >Router</a></li>
+    <li<?= $activate('manual-controller')?>><a href="#" >Controller</a></li>
+    <li<?= $activate('manual-performance')?>><a href="#" >Performance</a></li>
+</ul>
+<?php $this->endSectionAs('sub-menu'); ?>
+
+<?=
+/**
+ * output contents.
+ */
+$this->getContent();?>
 
