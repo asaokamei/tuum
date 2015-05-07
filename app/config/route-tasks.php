d56be8578aa742f6cfc69a9869521d4146a77fde
Asao <kamei@workspot.jp>
2015-05-07 17:03:06 +0900

    use release handle to set up view directory for tasks.

--- app/config/route-tasks.php
+++ app/config/route-tasks.php
@@ -1,14 +1,10 @@
 <?php
 
-use Tuum\View\Renderer;
-use Tuum\Web\Application;
 use Tuum\Web\Stack\RouterStack;
 use Tuum\Web\Web;
 
 /** @var Web $web */
-/** @var Application $app */
 /** @var RouterStack $stack */
-/** @var Renderer $views */
 
 /**
  * configure RouterStack for DemoTask.
@@ -16,16 +12,14 @@ use Tuum\Web\Web;
  * set root directory for the demo application using {*}
  * (so that the app does not have to know the root directory).
  */
-$stack  = $web->getRouterStack();
-
-$task_dir   = dirname(dirname(__DIR__)) . '/src/Tasks';
-$app->configure(
-    $task_dir . '/scripts/getRouterStack', [
-        'web'  => $web,
-        'stack' => $stack,
-        'view_dir' => null,
+$stack = $web->configure(
+    dirname(dirname(__DIR__)) . '/src/Tasks/scripts/getRouterStack',
+    [
+        'view_dir' => dirname(dirname(__DIR__)) . '/src/Tasks/views',
     ]
 );
+
+// set its root.
 $stack->setRoot('/demoTasks{*}');
 
 return $stack;
