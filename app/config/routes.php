6ddaa407333608043000e3047f63a17dc4897262
Asao <kamei@workspot.jp>
2015-08-27 16:24:02 +0900

    make Pagination class as the main usage; add more methods to use it alone.

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,22 +1,21 @@
 <?php
 /**
  * routes.php
- * 
- * configures routes for the web application. 
- * 
- * receives $stack as RouterStack to start. 
+ *
+ * configures routes for the web application.
+ *
+ * receives $stack as RouterStack to start.
  * must return the $stack to push to the stack.
- * 
+ *
  */
 use Demo\Site\SampleController;
-use Tuum\Pagination\Html\ToHtmlBootstrap;
+use Tuum\Pagination\Factory\Pagination;
 use Tuum\Router\RouteCollector;
 use Tuum\Web\Psr7\Request;
 use Tuum\Web\Stack\RouterStack;
 use Tuum\Web\Application;
 use Tuum\Web\Web;
 use Tuum\Pagination\Html\PaginateFull;
-use Tuum\Pagination\Html\PaginateMini;
 use Tuum\Pagination\Inputs;
 
 /** @var Web $web */
@@ -25,7 +24,7 @@ use Tuum\Pagination\Inputs;
 /** @var RouteCollector $routes */
 
 /**
- * start routing using RouteCollection object, $routes. 
+ * start routing using RouteCollection object, $routes.
  */
 
 $stack  = $web->getRouterStack();
@@ -33,25 +32,30 @@ $routes = $stack->getRouting();
 
 // main root
 
-$routes->get( '/', function($request) {
+$routes->get('/', function ($request) {
     /** @var Request $request */
     return $request->respond()->asView('index');
 });
 
-$routes->get('/pages', function(Request $request) {
+$routes->get('/pages', function (Request $request) {
 
     $pager = new Tuum\Pagination\Pager(['_limit' => 5]);
-    $pager = $pager->withRequest($request);
-    $inputs = $pager->call(function(Inputs $inputs) {
-        $inputs->setTotal(70);
-    });
-    $toHtml = ToHtmlBootstrap::forge();
-    $page1 = $toHtml->withPaginate(PaginateFull::forge()->withInputs($inputs));
-    $page2 = $toHtml->withPaginate(PaginateMini::forge()->withInputs($inputs));
+    $page1 = Pagination::forge($pager, new PaginateFull())
+        ->call($request,
+            function (Inputs $inputs) {
+                $inputs->setTotal(70);
+            });
+    $page2 = Pagination::forge($pager)
+        ->callQuery(
+            $request->getQueryParams(),
+            $request->getUri()->getPath(),
+            function (Inputs $inputs) {
+                $inputs->setTotal(70);
+            });
     return $request->respond()
         ->with('page1', $page1)
         ->with('page2', $page2)
-        ->with('_limit', $inputs->getLimit())
+        ->with('_limit', $page1->getLimit())
         ->asView('/pages');
 });
 
@@ -63,24 +67,24 @@ $routes->group(
     [
         'pattern' => '/closure',
     ],
-    function($routes) {
+    function ($routes) {
         /** @var RouteCollector $routes */
 
-        $routes->get( '', function($request) {
+        $routes->get('', function ($request) {
             /** @var Request $request */
             return $request->respond()->asHtml('<html><body><h1>This is from a closure!</h1></body></html>');
         });
 
-        $routes->get( '/view', function($request) {
+        $routes->get('/view', function ($request) {
             /** @var Request $request */
             return $request->respond()->asView('closure-view');
         });
 
-        $routes->get( '/text', function() {
+        $routes->get('/text', function () {
             return 'a text from closure';
         });
 
-        $routes->get( '/array', function() {
+        $routes->get('/array', function () {
             return ['arrays' => 'json'];
         });
 
@@ -90,14 +94,14 @@ $routes->group(
  * add sample controller route
  */
 
-$routes->any( '/sample{*}', SampleController::class);
+$routes->any('/sample{*}', SampleController::class);
 
 
 /** ---------------------------------------------------
- * finished routing. 
- * 
- * must return the router stack from routes file. 
- * or return null not to push to the middleware stack. 
+ * finished routing.
+ *
+ * must return the router stack from routes file.
+ * or return null not to push to the middleware stack.
  */
 
 return $stack;
